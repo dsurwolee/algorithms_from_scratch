c72d44091b124785be47df32a146ff6ed13cb821
dsurwolee <daniellee@C02S2239FVH8.local>
2017-12-13 22:31:00 -0500

    changes to binary_search script

--- cs_fundamentals/binary_search.py
+++ cs_fundamentals/binary_search.py
@@ -8,44 +8,46 @@ def midpoint(array):
 	""" Find the midpoint of a binary tree 
 	"""
 
-	min_i = 0
-	max_i = len(array)
-	mid_i = (max_i + min_i) / 2 
-	if max_i % 2 == 0:   
-		mid_r round(mid_i)
-		mid_l = mid_r - 1
-		mid_pr = array[mid_r] 
-		mid_pl = array[mid_l]
-		mid_p = (mid_pr + mid_pl) / 2
-	else:
-		mid_p = array[mid_i]      
-	return mid_p, mid_i
+	size = len(array)
+	if (size % 2) == 0: # Even
+		upper_i = int(size / 2)
+		lower_i = upper_i - 1
+		mid_i   = (upper_i + lower_i) / 2
+		mid_p = (array[lower_i] + array[upper_i]) / 2
+	else: # Odd
+		mid_i = int((size - 1) / 2)
+		mid_p = array[mid_i]
+	return mid_p, mid_i 
  
-def binary_search(key, array, _min, _max):
-	"""
-	"""
+# def binary_search(key, array):
+# 	""" Finds key by splitting the array in halves
+
+# 	Steps:
+# 		1) Find the midpoint, m, of an array 
+# 		2) If key greater than midpoint, update min index to m + 1.
+# 		   Otherwise, update the max index to m - 1.
+# 		3) Continue until midpoint found
+# 	"""
+
+# 	min_i, max_i = 0
+# 	while True:
+# 		mid_p, mid_i = midpoint(array)
 	
-	mid_p, mid_i = midpoint(array)
-	
-	if mid_p < key:
-		_min = mid_i + 1
-	else:
-		_max = mid_i - 1
+# 	if mid_p < key:
+# 		_min = mid_i + 1
+# 	else:
+# 		_max = mid_i - 1
 
-	array = array[_min:_max]
-	if len(array) > 1:
-		return array[_min:_max]
-	else: 
-		return mid_i
+# 	array = array[_min:_max]
+# 	if len(array) > 1:
+# 		return array[_min:_max]
+# 	else: 
+# 		return mid_i
 
 
-if __name__ == "__main__":
-	array = [2, 4, 6, 7, 10, 12, 15]
-	key = 12
+# if __name__ == "__main__":
+# 	array = [2, 4, 6, 7, 10, 12, 15]
+# 	key = 12
 
-	min_i = 0
-	max_i = len(array)
-	
-	cont = True
-	while cont:
-		array = binary_search(array, key, min_i, min_i)
+# 	key_found = binary_search(key=key, array=array)
+# 	print(key_found)
---
+++ cs_fundamentals/insertion_sort.py
@@ -0,0 +1,37 @@
+######################################
+# Author: Daniel S. Lee
+# Date: 12-09-17
+# Note: Binary Sorting in Python
+######################################
+
+def shift_values(array, key_i, j):
+	tmp = array[key_i]
+	for pos in range(j, key_i):
+		array[pos+1] = array[pos]
+	array[j] = tmp
+	return array
+
+def insertion_sort(array):
+	size = len(array)
+	for key_i in range(1, size):
+		for j in range(key_i-1,-1,-1):
+			if array[j] > array[key_i]: 
+				if j == 0:
+					array = shift_values(array, key_i, j)
+				elif array[j-1] <  array[key_i]:
+					array = shift_values(array, key_i, j)
+ 	return array
+
+# print(insertion_sort([2,1,3]))
+# print(insertion_sort([5,6,2,3,1]))
+# print(insertion_sort([5,6,3,2,1]))
+# print(insertion_sort([2,3,4,1]))
+# print(insertion_sort([1,2,5,3]))
+# print(insertion_sort([5,4,3,2]))
+print(insertion_sort([1,2,5,4]))
+print(insertion_sort([1,2,5,6,4]))
+
+print(insertion_sort([5,4,3,2,2]))
+
+#  5, 4, 3, 2
+#  1, 5, 3, 2
