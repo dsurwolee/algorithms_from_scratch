8bf905094f3e731458f03da0fcbc06c9a555142b
dsurwolee <daniellee@C02S2239FVH8.local>
2017-12-15 12:10:05 -0500

    insertion_sort completed

--- cs_fundamentals/insertion_sort.py
+++ cs_fundamentals/insertion_sort.py
@@ -1,31 +1,27 @@
 ######################################
 # Author: Daniel S. Lee
 # Date: 12-09-17
-# Note: Binary Sorting in Python
+# Note: Insertion Sorting in Python
 ######################################
 
-def shift_values(array, key_i, j):
-	tmp = array[key_i]
-	for pos in range(j, key_i):
-		array[pos+1] = array[pos]
-	array[j] = tmp
+def swap(array, key_i, j):
+	"""Swaps value in an array"""
+	temp = array[key_i]
+	array[key_i] = array[j] 
+	array[j] = temp
 	return array
 
 def insertion_sort(array):
-	size = len(array)
-	for key_i in range(1, size):
+	"""For each element staring in i = 0 to i = n + 1,
+	   swap with elements to the left occurs until the value 
+	   is less than or i = 0. 
+	"""
+	for key_i in range(1, len(array)):
 		for j in range(key_i-1,-1,-1):
-			if array[j] > array[key_i]: 
-				if j == 0:
-					array = shift_values(array, key_i, j)
-				elif array[j-1] <  array[key_i]:
-					array = shift_values(array, key_i, j)
- 	return array
-
-print(insertion_sort([1,2,5,4]))
-print(insertion_sort([1,2,5,6,4]))
-
-print(insertion_sort([5,4,3,2,2]))
-
-#  5, 4, 3, 2
-#  1, 5, 3, 2
+			print(key_i, j)
+			if array[j] > array[key_i]:
+				array = swap(array, key_i, j)
+				key_i = key_i - 1
+			else:
+				break
+	return array
