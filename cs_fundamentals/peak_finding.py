cb0fbb6f6968da9330223fa26b1aba8513878a81
dsurwolee <daniellee@C02S2239FVH8.local>
2017-12-19 11:44:48 -0500

    added comments to peak finding

--- cs_fundamentals/peak_finding.py
+++ cs_fundamentals/peak_finding.py
@@ -2,14 +2,31 @@
 # Author: Daniel S. Lee
 # Date: 12-09-17
 # Note: Peak finding algorithm using binary_search
-
-# https://codereview.stackexchange.com/questions/145224/1d-peak-finder-algorithm
 ######################################
  
 
 def peak_finder(a):
-	""" Binary split until peak found 
+	"""Divide and conquer approach to finding a peak
+		
+	   Finds the peak value in a list of numerical values
+	   using the divide and conquer approach. A value is a peak 
+	   if it's greater or equal to the values of its neighbors.
+
+	   Pseudocode: 
+	       1) If a[mid] >= a[left],
+	       	  	reduce list from the mid to last elements
+	       2) If a[mid] < a[right],
+	       		reduce list from the first to mid-1 elements
+		   3) If mid is a peak and mid > mid[right] or mid[left],
+		   		mid is a peak 
+	
 		Worst-case: O(log n)
+
+		Args:
+			a: List of integer values
+
+		Returns: 
+			a: Reduced list of integer values
 	"""
 
 	size = len(a)
